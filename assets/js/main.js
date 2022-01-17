a29b77cf135c4482978a54e880d7495f5eb18f68
hoangdung2812 <hoangdung28121999@gmail.com>
2022-01-17 17:00:29 +0700

    Second Task

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SHOW CART ===============*/
+
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+// Previously selected topic (if user selected)
+
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+
+// We validate if the user previously chose a topic
+
+
+// Activate / deactivate the theme manually with the button
